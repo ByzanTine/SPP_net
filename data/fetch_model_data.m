9a2f0576bd72e90e1b3ac1a30c158f901ff2e91f
ByzanTine <morbyzantine@gmail.com>
2014-10-27 20:11:22 -0400

    fetch_data_channel changed to the new one

--- data/fetch_model_data.m
+++ data/fetch_model_data.m
@@ -3,13 +3,12 @@ cur_dir = pwd;
 cd(fileparts(mfilename('fullpath')));
 
 fprintf('Downloading SPP_net_release1_data...\n');
-urlwrite('https://onedrive.live.com/download?resid=4006CBB8476FF777!9459&authkey=!ALvSvUfuMLHmCoA&ithint=file%2czip', ...
-    'SPP_net_release1_data.zip');
-
+urlwrite('https://onedrive.live.com/download?resid=4006CBB8476FF777!9723&authkey=!APTWXLD_P7UN6P0&ithint=file%2czip', ...
+    'SPP_net_release1_data_for_new_caffe.zip');
 fprintf('Unzipping...\n');
-unzip('SPP_net_release1_data.zip', '../');
+unzip('SPP_net_release1_data_for_new_caffe.zip', '../');
 
 fprintf('Done.\n');
-system('del SPP_net_release1_data.zip');
+%system('del SPP_net_release1_data.zip');
 
 cd(cur_dir);
