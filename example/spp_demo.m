32a04e888e4646c4b5292a5be0ca34a111fed287
tsechiw <tsechiw@deeplearn1.eecs.umich.edu>
2014-10-31 11:49:59 -0400

    convert image to conv_feature

--- example/spp_demo.m
+++ example/spp_demo.m
@@ -1,5 +1,7 @@
-cnn_definition_file 		= '/mnt/neocortex3/scratch/yejiayu/SPP_net_intu/finetuning/Zeiler_conv5_ft(5s_flip)/pascal_finetune_fc_spm_train_test_new.prototxt';
-cnn_binary_file 			= '/mnt/neocortex3/scratch/yejiayu/SPP_net_intu/finetuning/Zeiler_conv5_ft(5s_flip)/FT_iter_12000';
+%cnn_definition_file 		= '/mnt/neocortex3/scratch/yejiayu/SPP_net_intu/finetuning/Zeiler_conv5_ft(5s_flip)/pascal_finetune_fc_spm_train_test_new.prototxt';
+%cnn_binary_file 			= '/mnt/neocortex3/scratch/yejiayu/SPP_net_intu/finetuning/Zeiler_conv5_ft(5s_flip)/FT_iter_12000';
+cnn_definition_file			= '/mnt/neocortex/scratch/tsechiw/SPP_net_old/data/cnn_model/Zeiler_conv5/Zeiler_spm_scale224_test_conv5.prototxt';
+cnn_binary_file				= '/mnt/neocortex/scratch/tsechiw/SPP_net_old/data/cnn_model/Zeiler_conv5/Zeiler_conv5';
 opts.spp_params_def         = fullfile(pwd, 'data', 'Zeiler_conv5_new', 'spp_config');
 opts.cache_name             = 'Zeiler_conv5_ft(5s_flip)_fc7';
 
@@ -8,4 +10,18 @@ opts.feat_cache             = 'Zeiler_conv5';
 spp_model = spp_load_model(spp_model, true);
 
 im = imread('./example/images/000084.jpg');
-dets = spp_detect(im, spp_model, -1);
+
+feat_cache = [];
+opts.spm_im_size = [480 576 688 874 1200];
+% convert image to feature output by conv5 layer
+d.feat = spp_features_convX(im, opts.spm_im_size, feat_cache, conf.use_gpu);
+
+% convert conv_feature to pooling feature
+% d.feat = spp_features_convX_to_poolX
+% convert pool_feature to fc feature
+d.feat = spp_poolX_to_fcX(d.feat, feat_opts.layer, spp_model, conf.use_gpu);
+d.feat = spp_scale_features(d.feat, feat_opts.feat_norm_mean);
+
+score = bsxfun(@plus, spp_model.detectors(f).W * d.feat, spp_model.detectors(f).B)';
+
+%dets = spp_detect(im, spp_model, -1);
